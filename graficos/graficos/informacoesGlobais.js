5d2957035cdb196bdd9abe0a32835731f691bc31
Julia Gabrielly Barbosa Kerscher <134062353+JuliiaGaby@users.noreply.github.com>
2024-09-10 11:51:26 -0300

    informacoesGlobais.js

---
+++ graficos/graficos/informacoesGlobais.js
@@ -0,0 +1,20 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/global-data.json';
+
+async function visualizeGlobalInformation() {
+    const res = await fetch(url);
+    const data = await res.json();
+    const connectedPeople = (data.total_connected_people / 1e9);
+    const worldPopulation = (data.total_people_world / 1e9);
+    const hours = parseInt(data.average_time);
+    const minutes = Math.round((data.average_time - hours) * 100);
+    const connectedPercentage = ((connectedPeople / worldPopulation) * 100).toFixed(2);
+
+    const paragraph = document.createElement('p');
+    paragraph.classList.add('charts-container__text');
+    paragraph.innerHTML = `Did you know that the world has <span>${worldPopulation} billion</span> people and that approximately <span>${connectedPeople} billion</span> are connected to some social network and spend on average <span>${hours} hours</span> and <span>${minutes} minutes</span> connected.<br>This means that approximately <span>${connectedPercentage}%</span> of people are connected to some social network.`;
+
+    const container = document.getElementById('charts-container');
+    container.appendChild(paragraph);
+}
+
+visualizeGlobalInformation();
