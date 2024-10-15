4e47c754f5e67278f96065273e543f6481709268
Julia Gabrielly Barbosa Kerscher <134062353+JuliiaGaby@users.noreply.github.com>
2024-10-15 11:58:33 -0300

    Update quantidadeUsuarios.js

--- graficos/graficos/quantidadeUsuarios.js
+++ graficos/graficos/quantidadeUsuarios.js
@@ -1,7 +1,7 @@
 import { getCSS, tickConfig } from "./common.js";
 
 async function userCountByNetwork() {
-    const url = 'https://raw.githubusercontent.com/JuliiaGaby/main/api/global-data.json'
+    const url = 'https://raw.githubusercontent.com/JuliiaGaby/009.5/main/api/numero-usuarios.json'
     const res = await fetch(url);
     const data = await res.json();
     const networkNames = Object.keys(data);
