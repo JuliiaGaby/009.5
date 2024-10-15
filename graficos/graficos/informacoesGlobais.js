427b7ffae24afc81d24e554837ef94e1d219227c
Julia Gabrielly Barbosa Kerscher <134062353+JuliiaGaby@users.noreply.github.com>
2024-10-15 12:00:21 -0300

    Update informacoesGlobais.js

--- graficos/graficos/informacoesGlobais.js
+++ graficos/graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/JuliiaGaby/009.5/main/api/numero-usuarios.json';
+const url = 'https://raw.githubusercontent.com/JuliiaGaby/009.5/main/api/informacoesGlobais.js';
 
 async function visualizeGlobalInformation() {
     const res = await fetch(url);
